9b6694427c49ec280df58f14109769ccaef79271
Derek Henderson <derek.henderson@remote-learner.net>
2020-08-12 19:34:56 -0400

    DRH-1: Updates to use instructors.

---
+++ settings.php
@@ -0,0 +1,62 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Setting page for questionaire module
+ *
+ * @package    mod
+ * @subpackage questionnaire
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+global $DB;
+if ($ADMIN->fulltree) {
+    $options = array(0 => get_string('no'), 1 => get_string('yes'));
+    $str = get_string('configusergraphlong', 'questionnaire');
+    $settings->add(new admin_setting_configselect('questionnaire/usergraph',
+                                    get_string('configusergraph', 'questionnaire'),
+                                    $str, 0, $options));
+    $settings->add(new admin_setting_configtext('questionnaire/maxsections',
+                                    get_string('configmaxsections', 'questionnaire'),
+                                    '', 10, PARAM_INT));
+    $choices = array(
+        'response' => get_string('response', 'questionnaire'),
+        'submitted' => get_string('submitted', 'questionnaire'),
+        'institution' => get_string('institution'),
+        'department' => get_string('department'),
+        'course' => get_string('course'),
+        'group' => get_string('group'),
+        'id' => get_string('id', 'questionnaire'),
+        'fullname' => get_string('fullname'),
+        'username' => get_string('username')
+    );
+
+    $settings->add(new admin_setting_configmultiselect('questionnaire/downloadoptions',
+            get_string('textdownloadoptions', 'questionnaire'), '', array_keys($choices), $choices));
+    
+    $roles = array();
+    $roles[0] = get_string('selectrole', 'questionnaire');
+    $rolelist = $DB->get_records('role');
+    foreach($rolelist as $role) {
+         $roles[$role->id] = $role->shortname;     
+    }
+    $settings->add(new admin_setting_configmultiselect('questionnaire/teacherrole',
+                                    get_string('teacherrole', 'questionnaire'),
+                                     '', array_keys($roles), $roles));
+            
+}
