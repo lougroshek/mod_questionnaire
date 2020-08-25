8cafb39cf8b25fd0422953246f76d1dfca9d9186
Derek Henderson <derek.henderson@remote-learner.net>
2020-08-25 08:09:59 -0400

    DRH-2: Updated demo.

--- demo.php
+++ demo.php
@@ -19,23 +19,18 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-
-//defined('MOODLE_INTERNAL') || die();
-ini_set('display_startup_errors', 1);
-ini_set('display_errors', 1);
-error_reporting(-1);
 include ('../../config.php');
 global $DB;
-$qid =  5;
-// get all the instructors;
+$qid = optional_param('qid', null, PARAM_INT);              // Survey id.
+
+// Get all the instructors;
 
 $queryins = "Select DISTINCT(c.staffid)
              FROM {questionnaire_question} q
              JOIN {questionnaire_quest_ins} c
               ON question_id = q.id
               WHERE q.surveyid =".$qid."
-              ORDER by c.staffid"              
-              ;
+              ORDER by c.staffid";
 
 $resultins = $DB->get_records_sql($queryins);
 $rowheaders = [];
@@ -46,7 +41,7 @@ foreach($resultins as $result) {
    $staff[] = $result->staffid;
    $userid = $result->staffid;
    $lname = $DB->get_field('user','lastname', array('id' => $userid));
-   $fname = $DB->get_field('user','firstname', array('id' => $userid)) . ' '.$lname;   
+   $fname = $DB->get_field('user','firstname', array('id' => $userid)) . ' '.$lname;
    $rowheaders[] = $fname;
    $staffcnt = $staffcnt + 1;
 }
@@ -62,7 +57,7 @@ $queryres = "SELECT CONCAT_WS('_', qr.id, 'ratescale', qrr.id) AS id, qr.submitt
                     qrr.rankvalue
              FROM   {questionnaire_response} qr
              JOIN   {questionnaire_response}_rank qrr
-               ON   qrr.response_id = qr.id AND qr.questionnaireid =".$qid ." 
+               ON   qrr.response_id = qr.id AND qr.questionnaireid =".$qid ."
                     AND qr.complete = 'y'
                     LEFT JOIN {user} u ON u.id = qr.userid
                     ORDER BY usrid, id";
@@ -77,51 +72,42 @@ foreach ($surveyres as $survey){
 	 $choiceid = $survey->choice_id;
 	 // Get the staff id.
 	 $staffid = $DB->get_field('questionnaire_quest_ins', 'staffid', array('id' => $choiceid));
-    $rankvalue = $survey->rankvalue;
-		
+         $rankvalue = $survey->rankvalue;
 	 $key = array_search($staffid, $staff);
-	 $cols[$key] = $rankvalue; 
+	 $cols[$key] = $rankvalue;
 	 $rankvalue = $survey->rankvalue;
-	 
   	 if ($olduser == 0) {
-	     $olduser = $survey->userid; 
+	     $olduser = $survey->userid;
 	 }
 
 	 if ($olduser <> $survey->userid) {
-        // display old values;
-        $ln = [];
-        $first = $first .' '.$last;
-        $ln = array($first);
-        for($k = 0; $k < $staffcnt; $k++) {
-        	  array_push($ln, $cols[$k]);
-           // Reset.
-           $cols[$k] = 0; 
-        }
-        $lnew[$cnt] = $ln;
-        $cnt = $cnt + 1;
-        $olduser = $survey->userid;
+             // display old values;
+             $ln = [];
+             $first = $first .' '.$last;
+             $ln = array($first);
+             for($k = 0; $k < $staffcnt; $k++) {
+                 array_push($ln, $cols[$k]);
+                 // Reset.
+                 $cols[$k] = 0;
+             }
+             $lnew[$cnt] = $ln;
+             $cnt = $cnt + 1;
+             $olduser = $survey->userid;
 	 } else {
 	     $last = $survey->lastname;
-	     $first = $survey->firstname;               	     
+	     $first = $survey->firstname;
 	 }
-	 
-//    echo '<br>'. $survey->userid. ' rank value '.$survey->rankvalue. ' choice '.$choiceid;
-    
 }
 $first = $first .' '.$last;
 $ln = array($first);
 for($k = 0; $k < $staffcnt; $k++ ) {
-	  array_push($ln, $cols[$k]);
+    array_push($ln, $cols[$k]);
 }
-$lines[] = $ln; 
+$lines[] = $ln;
 $lnew[$cnt] = $ln;
 
-//$lnew = [];
-//$lnew[0] = array(0 =>"demo1", 1 => "french");
-
 $filename = '123';
-//exit();
 
  // Use Moodle's core download function for outputting csv.
-\core\dataformat::download_data($filename, 'csv', $rowheaders, $lnew); 
-   
+\core\dataformat::download_data($filename, 'csv', $rowheaders, $lnew);
+
